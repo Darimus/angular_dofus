5de8c29921dc193d5ca071f78e2f8abd80fc8543
Darimus <bruno.passos@outlook.com.br>
2023-06-09 18:27:25 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+
+
 import { AppComponent } from './app.component';
+import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
+import { InputCalculadoraComponent } from './componentes/calculadora/input-calculadora/input-calculadora.component';
+import { RodapeComponent } from './componentes/rodape/rodape.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabecalhoComponent,
+    InputCalculadoraComponent,
+    RodapeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/componentes/calculadora/input-calculadora/input-calculadora.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-input-calculadora',
+  templateUrl: './input-calculadora.component.html',
+  styleUrls: ['./input-calculadora.component.scss']
+})
+export class InputCalculadoraComponent {
+
+}
